b10c5df7cc8f632f763f0ec503f4bcebc2143019
Swag2026 <it.dept@swag.com.sa>
2026-07-12 11:21:41 +0300

    Remove backend directory

--- swag-gold-project/backend/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .user import Role, User
-from .contact import Contact
-from .invoice import Invoice, InvoiceItem
-from .supplier_payment import SupplierPayment
-from .expense import Expense
-from .cost_rate import CostRate
-from .activity_log import ActivityLog
-from .app_setting import AppSetting
